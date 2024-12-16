1dc76e2c84a74baadcec88b2628d1891099e52f2
Jayasurya B M <inboxofsurya@gmail.com>
2024-12-16 09:51:24 +0530

    Initial commit

---
+++ Problem1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projecteuler
+{
+    internal class Problem1
+    {
+        public static int SumOfMultiplesOf3or5()
+        {
+            int sum = 0;
+            for (int loop = 1; loop < 1000; loop++)
+            {
+                if(loop%3==0 || loop % 5 == 0)
+                {
+                    sum += loop;
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace projecteuler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(Problem1.SumOfMultiplesOf3or5());
+            Console.ReadLine();
+        }
+    }
+}
